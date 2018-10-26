f1485b353d53378a052e4f79c0e07223896964b5
Ing.StebenAragon <stebenvera@gmail.com>
2018-10-26 09:10:16 -0500

    ultimos cambios punto 2

--- Taller2_JS/js/punto_dos.js
+++ Taller2_JS/js/punto_dos.js
@@ -6,44 +6,50 @@ var btn_eleccion = document.getElementById("eleccion");
 
 
 
-//Seccion para crear elementos
- var array_divs = [];
+//Seccion para crear variables 
  var contador = 0;
- var elemento_div = document.createElement("div");
- var elemento_h = document.createElement("h1");
- var contenido = document.createTextNode("Soy nuevo div");
+
+
+ //var contenido = document.createTextNode("Soy nuevo div");
  //array_divs[0] = elemento_div = document.createElement("div");
  // array_divs[1] = elemento_div = document.createElement("div");
- elemento_h.appendChild(contenido);
+// elemento_h.appendChild(contenido);
 
  //Seccion botones y eventos
 
 function agregar() {
   //  div_padre.appendChild(array_divs[0]);
    // div_padre.appendChild(array_divs[1]);
-        
+        /*
         contador++;  
         console.log(contador);   
         array_divs[contador] =document.createElement("div");
-        div_padre.appendChild(array_divs[contador]);
+        div_padre.appendChild(array_divs[contador]);*/
+        contador++;
+        var elemento_div = document.createElement("div");
+        elemento_div.setAttribute("class","d");
+        var elemento_h = document.createElement("h1");
+        elemento_h.innerHTML="Div "+contador;
+        div_padre.appendChild(elemento_div);
+        elemento_div.appendChild(elemento_h);
+        
+        console.log(contador);
         
 }
-
 function quitar() {
-        
-        
-              for ( cont = array_divs.length-1; cont >=0; cont--) {
-                
-                div_padre.removeChild(array_divs[cont]);
-                contador = 0;
-                }
-                
-                console.log(contador); 
+    while (div_padre.hasChildNodes()) {
+        div_padre.removeChild(div_padre.childNodes[0]);
+    }
+    contador = 0;
+    console.log(contador);
 }
 
 function eleccion() {
         var opc =parseInt(prompt("Ingrese el numero del Div que desea borrar"));
-        div_padre.removeChild(array_divs[opc]);
+        div_padre.removeChild(div_padre.childNodes[opc-1]);
+        console.log(contador);
+        
+        
 }
 
  btn_agregar.addEventListener("click",agregar);
