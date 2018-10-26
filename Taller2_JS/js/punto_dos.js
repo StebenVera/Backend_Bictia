8beb193768fcff8591ed1ecd43f284e0cf917740
Ing.StebenAragon <stebenvera@gmail.com>
2018-10-25 20:09:33 -0500

    taller continucacion punto2

--- Taller2_JS/js/punto_dos.js
+++ Taller2_JS/js/punto_dos.js
@@ -1,6 +1,7 @@
 var div_padre = document.getElementById("principal");
 var btn_agregar = document.getElementById("agregar");
 var btn_quitar = document.getElementById("quitar");
+var btn_eleccion = document.getElementById("eleccion");
 
 
 
@@ -9,9 +10,11 @@ var btn_quitar = document.getElementById("quitar");
  var array_divs = [];
  var contador = 0;
  var elemento_div = document.createElement("div");
+ var elemento_h = document.createElement("h1");
+ var contenido = document.createTextNode("Soy nuevo div");
  //array_divs[0] = elemento_div = document.createElement("div");
  // array_divs[1] = elemento_div = document.createElement("div");
-
+ elemento_h.appendChild(contenido);
 
  //Seccion botones y eventos
 
@@ -27,16 +30,22 @@ function agregar() {
 }
 
 function quitar() {
-  
-        for ( cont = array_divs.length-1; cont >0; cont--) {
-            
-            div_padre.removeChild(array_divs[cont]);
-            contador = 0;
-        }
         
-        console.log(contador); 
-    
+        
+              for ( cont = array_divs.length-1; cont >=0; cont--) {
+                
+                div_padre.removeChild(array_divs[cont]);
+                contador = 0;
+                }
+                
+                console.log(contador); 
+}
+
+function eleccion() {
+        var opc =parseInt(prompt("Ingrese el numero del Div que desea borrar"));
+        div_padre.removeChild(array_divs[opc]);
 }
 
  btn_agregar.addEventListener("click",agregar);
  btn_quitar.addEventListener("click",quitar);
+ btn_eleccion.addEventListener("click",eleccion);
